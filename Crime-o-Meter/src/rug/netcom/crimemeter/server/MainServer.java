807171ee4e36861b6682f76833c0363450c51076
Blue-Motion <a.offereins+github@student.rug.nl>
2014-03-25 20:54:25 +0100

    First run on adding central server

---
+++ Crime-o-Meter/src/rug/netcom/crimemeter/server/MainServer.java
@@ -0,0 +1,14 @@
+package rug.netcom.crimemeter.server;
+
+import rug.netcom.crimemeter.server.socket.TCPServer;
+
+public class MainServer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//RMIServer.init();
+		//TCPServer.init();
+
+	}
+
+}
